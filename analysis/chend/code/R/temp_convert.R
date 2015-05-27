10ffda3602b844f9d525c7efa6eccdafdb966247
Daniel Chen <Maveri2k@yahoo.com>
2015-05-27 14:30:17 -0400

    f_to_c function with test for f_to_k

--- analysis/chend/code/R/temp_convert.R
+++ analysis/chend/code/R/temp_convert.R
@@ -1,8 +1,10 @@
+library(testthat)
+
 f_to_k <- function(temp) {
 	return((temp - 32) * (5 /9) + 273.15)	
 }
 
-f_to_k(32)
+expect_equal(f_to_k(32), 273.15)
 
 
 k_to_c <- function(temp) {
@@ -11,3 +13,10 @@ k_to_c <- function(temp) {
 
 k_to_c(0)
 
+f_to_c <- function(temp) {
+	k_convert <- f_to_k(temp)
+	c_convert <- k_to_c(k_convert)
+	return(c_convert)
+}
+
+f_to_c(32)
